39c428d743bf2252ed821903aac6fbf9abb7f921
Júlio César <juliocholiveira@gmail.com>
2020-06-06 16:14:57 -0300

    adiciona o cors ao servidor

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -10,6 +11,10 @@ import AppError from './errors/AppError';
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:3333'
+}));
 
 // Servindo arquivos estáticos
 app.use('/files', express.static(uploadConfig.directory));
